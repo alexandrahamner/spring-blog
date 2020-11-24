17cc136b3074fb09efdbd32326aaa83d467e8069
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-24 10:43:05 -0600

    feat: completed post controller.

---
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts() {
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postsIndex(@PathVariable Long id) {
+        return "view an individual post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewCreateForm() {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "create a new post";
+    }
+}
