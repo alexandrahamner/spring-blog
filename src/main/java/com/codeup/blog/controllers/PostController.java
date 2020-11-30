eafe0d7bd51f24c037c649d7be2c9e3c3f50a303
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-30 09:59:59 -0600

    feat: show now displays an individual post.

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -25,9 +25,11 @@ class PostController {
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable Long id) {
-        return "view an individual post";
+    public String individualPost(@PathVariable Long id, Model model) {
+        Post post = new Post("For Loops in Javascript", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit");
+
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
