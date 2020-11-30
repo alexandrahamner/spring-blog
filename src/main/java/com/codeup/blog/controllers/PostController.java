87bfba45459fe61efbcf721db27cb4ca1b580ee1
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-11-30 09:22:14 -0600

    feat: index now shows an arraylist of posts.

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,18 +1,27 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String postsIndex() {
-        return "index";
+    public String postsIndex(Model model) {
+        List<Post> postIndex = new ArrayList<>();
+        postIndex.add(new Post("For Loops in Javascript", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+        postIndex.add(new Post("While Loops in Javascript", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+
+        model.addAttribute("postIndex", postIndex);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
