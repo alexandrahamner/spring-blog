77ad02ce91d504140ec0eaa0e31f30541aa47d4a
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-12-07 11:49:11 -0600

    feat: sign-up and login page are set up with the necessary controllers.

--- src/main/java/com/codeup/blog/controllers/UserController.java
+++ src/main/java/com/codeup/blog/controllers/UserController.java
@@ -3,11 +3,13 @@ package com.codeup.blog.controllers;
 import com.codeup.blog.models.User;
 import com.codeup.blog.repos.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UserController {
     private final UserRepository usersDao;
     private final PasswordEncoder passwordEncoder;
